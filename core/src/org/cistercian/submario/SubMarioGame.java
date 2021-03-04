8da9e194ddd34e6ffb93774a52366bd3eb9f0b56
Henry Parmenter <21parmenterh@cistercian.org>
2021-03-04 13:49:12 -0600

    Coral Corners

--- core/src/org/cistercian/submario/SubMarioGame.java
+++ core/src/org/cistercian/submario/SubMarioGame.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SubMarioGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture crateImg, snowImg, redBrickImg, brownBrickImg, coralImg;
+	Texture crateImg, snowImg, redBrickImg, brownBrickImg, coralImg, lCoralImg, rCoralImg;
 	Texture rSeahorseImg, lSeahorseImg;
 	Player seahorse;
 	Map gameMap = new Map();
@@ -20,6 +20,8 @@ public class SubMarioGame extends ApplicationAdapter {
 		// load images
 		crateImg = new Texture("crate.png");
 		coralImg = new Texture("coral.png");
+		lCoralImg = new Texture("leftCoral.png");
+		rCoralImg = new Texture("rightCoral.png");
 		snowImg = new Texture("snow.png");
 		redBrickImg = new Texture("red_brick.png");
 		brownBrickImg = new Texture("brown_brick.png");
@@ -41,10 +43,11 @@ public class SubMarioGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5294f, 0.8078f, 0.98f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		for(int i = 0; i < 30; i ++){
+		for(int i = 1; i < 31; i ++){
 			batch.draw(coralImg, 25*i, 0);
 		}
-		batch.draw(coralImg, 500, 0);
+		batch.draw(lCoralImg, 0, 0);
+		batch.draw(rCoralImg, 31*25, 0);
 		seahorse.draw(batch);
 		batch.end();
 	}
